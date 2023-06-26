1e1dafca84984057ad7c620dc77ae1b9a8f035a4
Bhoomikachakilam <122263678+Bhoomikachakilam@users.noreply.github.com>
2023-06-26 23:17:14 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import connectDB from "./config/db.js"
 dotenv.config()
 connectDB()
-const port = process.env.PORT;
+const port = process.env.PORT||5000;
 const app = express()
 app.use(cors());
 app.use(express.json())
